fd158c457736567725f6a81d0160a04a794c108d
gaz honsepaskwa <gaz.honsepaskwa@gmail.com>
2024-01-18 09:13:15 +0100

    tmp

---
+++ C__CodePython/Banque.py
@@ -0,0 +1,52 @@
+"""
+Qui: Nathan Amory
+Quand: 2021-05-23
+Description: 
+
+"""
+
+
+##########
+# import #
+##########
+
+
+class Nom():
+
+    ################
+    # constructeur #
+    ################
+
+    def __init__(self, nom):
+        self.__nom = nom
+
+
+
+    #############
+    # affichage #
+    #############
+        
+    def __str__(self):
+        print("")
+        
+    
+
+    ###########################
+    # accesseurs et mutateurs #
+    ###########################
+        
+    @property
+    def nom(self):
+        pass
+    @nom.setter
+    def nom(self, nom):
+        self.__nom = nom
+
+
+
+    ############
+    # methodes #
+    ############
+    
+    def methode(self):
+        pass
