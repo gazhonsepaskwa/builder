892c2cb4c849a8cf700ce39b6ef1a35be5a06217
gazhonsepaskwa <101428905+gazhonsepaskwa@users.noreply.github.com>
2024-05-16 23:17:46 +0200

    recup depuis python en cours + correction sur site statique

--- C__CodePython/Plateau.py
+++ C__CodePython/Plateau.py
@@ -61,7 +61,7 @@ class Plateau(Entity):
     # permet de genere les cases du jeu
     def genererCases(self):
 
-        # se prendra de la DB en temps voulu
+        """
         c0 = Case           (0, (4, 0, -4),     couleur=color.green             )
         c1 = Case_propriete (1, (3, 0, -4),     "Nom a.1", 60, 10,  (2,0,0), 1  )
         c2 = Case_ressource (2, (2, 0, -4),     "camion" ,                      )
@@ -94,35 +94,16 @@ class Plateau(Entity):
         c29= Case_propriete (29,(4, 0, -1),     "Nom h.1", 400, 10, (-1,0,-1), 8)
         c30= Case_chance    (30,(4, 0, -2)                                      )
         c31= Case_propriete (31,(4, 0, -3),     "Nom h.2", 400, 10, (0,0,-2), 8 )
+        """
 
-        self.caseListe.append(c1)
-        self.caseListe.append(c2)
-        self.caseListe.append(c3)
-        self.caseListe.append(c4)
-        self.caseListe.append(c5)
-        self.caseListe.append(c6)
-        self.caseListe.append(c7)
-        self.caseListe.append(c8)
-        self.caseListe.append(c9)
-        self.caseListe.append(c10)
-        self.caseListe.append(c11)
-        self.caseListe.append(c12)
-        self.caseListe.append(c13)
-        self.caseListe.append(c14)
-        self.caseListe.append(c15)
-        self.caseListe.append(c16)
-        self.caseListe.append(c17)
-        self.caseListe.append(c18)
-        self.caseListe.append(c19)
-        self.caseListe.append(c20)
-        self.caseListe.append(c21)
-        self.caseListe.append(c22)
-        self.caseListe.append(c23)
-        self.caseListe.append(c24)
-        self.caseListe.append(c25)
-        self.caseListe.append(c26)
-        self.caseListe.append(c27)
-        self.caseListe.append(c28)
-        self.caseListe.append(c29)
-        self.caseListe.append(c30)
-        self.caseListe.append(c31)
+        for i in range(0, 31):
+
+            parametre = ''
+
+            if type_case == "Case": parametre = 'couleur=color.green'
+            elif type_case == "Case_propriete": parametre = f'"{nom_propriete}", {prix}, {loyer},  {pos_batt}, {num_hamaux}'
+            elif type_case == "Case_ressource": parametre = f'"{resource_contenue}" '
+            else : parametre = ''
+
+            exec(f"c{i} = {type_case} ({i}, {case_position}, {parametre} )")
+            exec(f"self.caseListe.append(c{i})")
